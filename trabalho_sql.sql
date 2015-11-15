e54a85fbe6997031235511f865497da5db49d20d
Victor Perin <me@victorperin.com>
2015-11-15 21:33:45 -0200

    i don't know exactly what I've done.

--- trabalho_sql.sql
+++ trabalho_sql.sql
@@ -1,12 +1,37 @@
-CREATE DATABASE IF NOT EXISTS teste;
-USE teste;
+CREATE DATABASE IF NOT EXISTS trabalho_lpbd;
+USE trabalho_lpbd;
 
 
 CREATE TABLE IF NOT EXISTS Albuns (
   Id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  Nome int(10) unsigned NOT NULL DEFAULT '0',
-  DataLancamento date NOT NULL,
+  Nome varchar(50) NOT NULL,
+  AnoLancamento int(10) NOT NULL,
   PRIMARY KEY (Id)
+);
+
+CREATE TABLE IF NOT EXISTS Genero (
+  Id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  Nome varchar(50) NOT NULL,
+  PRIMARY KEY (Id),
+  KEY Nome (Nome)
+);
+
+-- Lista de países.
+CREATE TABLE IF NOT EXISTS Paises (
+  Id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  Nome varchar(50) NOT NULL,
+  PRIMARY KEY (Id),
+  KEY SelectByNome (Nome)
+);
+
+CREATE TABLE IF NOT EXISTS Artistas (
+  Id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  Nome varchar(50) NOT NULL,
+  GeneroId int(10) unsigned NOT NULL,
+  CountryId int(10) unsigned NOT NULL,
+  PRIMARY KEY (Id),
+  CONSTRAINT genero_Artistas FOREIGN KEY (GeneroId) REFERENCES Genero (Id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT contry_Artistas FOREIGN KEY (CountryId) REFERENCES Paises (Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Relação MANY TO MANY de Albuns e Artistas
@@ -18,21 +43,6 @@ CREATE TABLE IF NOT EXISTS AlbunsArtistas (
   KEY album_AlbunsArtistas (AlbumId),
   CONSTRAINT album_AlbunsArtistas FOREIGN KEY (AlbumId) REFERENCES Albuns (Id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT artista_AlbunsArtistas FOREIGN KEY (ArtistaId) REFERENCES Artistas (Id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS Artistas (
-  Id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  Nome varchar(50) NOT NULL,
-  Genero int(10) unsigned NOT NULL,
-  Country int(11) DEFAULT NULL,
-  PRIMARY KEY (Id)
-);
-
-CREATE TABLE IF NOT EXISTS Genero (
-  Id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  Nome varchar(50) NOT NULL,
-  PRIMARY KEY (Id),
-  KEY Nome (Nome)
 );
 
 CREATE TABLE IF NOT EXISTS Musicas (
@@ -51,20 +61,11 @@ CREATE TABLE IF NOT EXISTS Musicas (
 );
 
 CREATE TABLE MusicasComArtistaEAlbum (
-
 	NomeMusica VARCHAR(100) NOT NULL,
 	NomeArtista VARCHAR(50) NOT NULL,
 	NomeAlbum INT(10) UNSIGNED NOT NULL,
 	DataLancamento DATE NOT NULL
 );
 
--- Lista de países.
-CREATE TABLE IF NOT EXISTS Paises (
-  Id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  Nome varchar(50) NOT NULL,
-  PRIMARY KEY (Id),
-  KEY SelectByNome (Nome)
-);
-
 DROP TABLE IF EXISTS MusicasComArtistaEAlbum;
-CREATE ALGORITHM=UNDEFINED DEFINER=root@localhost SQL SECURITY DEFINER VIEW MusicasComArtistaEAlbum AS select Musicas.Nome AS NomeMusica,Artistas.Nome AS NomeArtista,Albuns.Nome AS NomeAlbum,Albuns.DataLancamento AS DataLancamento from (((Musicas join Albuns on((Musicas.AlbumId = Albuns.Id))) join AlbunsArtistas on((AlbunsArtistas.AlbumId = Albuns.Id))) join Artistas on((AlbunsArtistas.ArtistaId = Artistas.Id)));
+CREATE ALGORITHM=UNDEFINED DEFINER=root@localhost SQL SECURITY DEFINER VIEW MusicasComArtistaEAlbum AS select Musicas.Nome AS NomeMusica,Artistas.Nome AS NomeArtista,Albuns.Nome AS NomeAlbum,Albuns.AnoLancamento AS DataLancamento from (((Musicas join Albuns on((Musicas.AlbumId = Albuns.Id))) join AlbunsArtistas on((AlbunsArtistas.AlbumId = Albuns.Id))) join Artistas on((AlbunsArtistas.ArtistaId = Artistas.Id)));
