c160248338d5db0b3f583cb2dcf3b6a026662444
Victor Perin <me@victorperin.com>
2015-11-16 02:38:34 -0200

    feat(consultas): inserindo primeiras consultas

---
+++ consultas.sql
@@ -0,0 +1,23 @@
+SELECT
+  Albuns.Id,
+  Albuns.Nome,
+  Albuns.AnoLancamento
+FROM Albuns
+  JOIN AlbunsArtistas ON AlbunsArtistas.AlbumId = Albuns.Id
+  JOIN Artistas ON Artistas.Id = AlbunsArtistas.Artista.Id
+WHERE
+  Artistas.Nome like "%Asking%";
+
+SELECT
+  NomeMusica,
+  NomeArtista
+FROM MusicasComArtistaEAlbum
+WHERE
+  NomeArtista = "Sleeping With Sirens";
+
+SELECT
+  NomeArtista
+  NomeAlbum,
+  COUNT(NomeMusica) AS QuantidadeDeMusicas
+FROM MusicasComArtistaEAlbum
+GROUP BY NomeAlbum;
